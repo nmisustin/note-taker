3b990b0ab902f4485786364a86cf7ee684343b6c
nmisustin <nmisustin@gmail.com>
2021-05-08 20:42:58 -0400

    added tests and functions to note.js

---
+++ __tests__/notes.test.js
@@ -0,0 +1,24 @@
+const {createNewNote, validateNote} = require("../lib/notes")
+const fs = require("fs");
+const db = require('../db/db.json')
+
+jest.mock('fs');
+
+test("test new note creation", () => {
+    const note = createNewNote(
+        {title: "test note", text: "this is the text"},
+        db
+    );
+    expect(note.title).toBe("test note");
+    expect(note.text).toBe("this is the text");
+})
+test("validate note", () => {
+    const note = {title: "test", text: "this is a string"}
+    const invalidNote = {title: 2, text: "invalid"}
+
+    const test = validateNote(note);
+    const test2 = validateNote(invalidNote);
+
+    expect(test).toEqual(true);
+    expect(test2).toEqual(false);
+})
